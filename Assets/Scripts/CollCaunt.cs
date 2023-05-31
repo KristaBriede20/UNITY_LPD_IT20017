b85347be965105d8cf64b61dcb1ac3dbac34d190
Krista <kristabriede01@gmail.com>
2023-05-31 18:56:43 +0300

    win end

--- Assets/Scripts/CollCaunt.cs
+++ Assets/Scripts/CollCaunt.cs
@@ -7,15 +7,25 @@ public class CollCaunt : MonoBehaviour
 {
     public Text ballCount;
     public static int count;
+    public GameObject winText;
+
 
     void Awake()
     {
         count = 0;
         ballCount.text = "Balls found: " + count;
+        winText.SetActive(false);
     }
     
     void Update()
     {
         ballCount.text = "Balls found " + count + " of 9";
+        if(count == 9)
+        {
+            winText.SetActive(true);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            Time.timeScale = 0f;
+        }
     }
 }
