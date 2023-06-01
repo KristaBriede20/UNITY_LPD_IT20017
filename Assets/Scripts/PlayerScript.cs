7fb7e7d6f19627556fe367b09a163b4377ff022d
Krista <kristabriede01@gmail.com>
2023-06-01 10:07:12 +0300

    real JUMP scare

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class PlayerScript : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 12f;
-
     public float gravity = -9.81f;
 
     public Transform groundCheck;
@@ -15,19 +15,26 @@ public class PlayerScript : MonoBehaviour
     public float jumpHeight = 4f;
     bool isGrounded;
 
+    public AudioClip jumpSound; 
+    private AudioSource jumpAudioSource; 
 
     Vector3 velocity;
 
     void Start()
     {
+        
+        GameObject jumpAudioObject = new GameObject("JumpAudio");
+        jumpAudioObject.transform.parent = transform; 
+        jumpAudioSource = jumpAudioObject.AddComponent<AudioSource>();
 
+        jumpAudioSource.clip = jumpSound; 
     }
 
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if(isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
@@ -35,19 +42,20 @@ public class PlayerScript : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+           
+            jumpAudioSource.PlayOneShot(jumpSound);
         }
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
-
     }
 }
